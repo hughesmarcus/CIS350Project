040b66e6fa86f237071fdc1d09f8d916b7e848e8
Marcus Hughes <marcushughes80@gmail.com>
2015-01-30 12:16:33 -0500

    real first commit

---
+++ Illness.java
@@ -0,0 +1,21 @@
+import java.awt.List;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Illness {
+public Illness{
+	String name;
+	ArrayList<String> my = new ArrayList<String>();
+}
+public String getname(String name) {
+	return name;
+}
+public void addsymptom(String symptom){
+	
+	my.add(symptom);
+	
+}
+
+};
