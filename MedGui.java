b8f9d0033bd7b2cfd037c50ca0663029b845e1fa
vincent-sj <vincent-sj@users.noreply.github.com>
2015-02-10 17:21:22 -0500

    Update MedGui.java

--- MedGui.java
+++ MedGui.java
@@ -6,7 +6,6 @@ import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
-
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
@@ -16,9 +15,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JButton;
-
 import org.eclipse.wb.swing.FocusTraversalOnArray;
-
 import javax.swing.JComboBox;
 import javax.swing.JList;
 import javax.swing.border.TitledBorder;
@@ -34,7 +31,8 @@ public class MedGui {
 	static JPanel recordPanel;
 	JPanel searchPanel;
 	static JPanel patientsPanel;
-	JPanel profilePanel;
+	static JPanel editPanel;
+	private JTextField addSympField;
 
 	/**
 	 * Launch the application.
@@ -69,7 +67,8 @@ public class MedGui {
 	public static void doctorView()
 	{
 		tabbedPane.addTab("Record", null, recordPanel, null);
-		tabbedPane.addTab("Patients", null, patientsPanel, null);
+		tabbedPane.addTab("Edit", null, editPanel, null);
+		//tabbedPane.addTab("Patients", null, patientsPanel, null);
 	}
 	
 	/**
@@ -78,13 +77,13 @@ public class MedGui {
 	private void initialize() {
 		frame = new JFrame("MediApp");
 		frame.setResizable(false);
-		frame.setBounds(100, 100, 398, 413);
+		frame.setBounds(100, 100, 450, 413);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.setFont(new Font("Dialog", Font.BOLD, 12));
 		tabbedPane.setToolTipText("");
-		tabbedPane.setBounds(2, 0, 388, 382);
+		tabbedPane.setBounds(2, 0, 442, 382);
 		frame.getContentPane().add(tabbedPane);
 		frame.setVisible(false);
 		
@@ -93,26 +92,71 @@ public class MedGui {
 		sympTxtArea.setWrapStyleWord(true);
 		
 		recordPanel = new JPanel();
+		editPanel = new JPanel();
+		editPanel.setLayout(null);
+		
+		//get list of ills
+		JList illList = new JList();
+		illList.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		illList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		illList.setBounds(10, 45, 150, 296);
+		editPanel.add(illList);
+		
+		addSympField = new JTextField();
+		addSympField.setBounds(170, 99, 148, 33);
+		editPanel.add(addSympField);
+		addSympField.setColumns(10);
+		
+		JButton btnAddSymptom = new JButton("Add Symptom");
+		btnAddSymptom.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//add the inputed symptom to the selected illness if it doesn't already have it
+				String newSymp = addSympField.getText();
+				Object selIll = illList.getSelectedValue();//this should always be a string since the sympList should only be holding an array of strings
+			}
+		});
+		btnAddSymptom.setBounds(328, 99, 99, 33);
+		editPanel.add(btnAddSymptom);
+		
+		JButton btnRemoveSymptom = new JButton("Remove Symptom");
+		btnRemoveSymptom.setBounds(279, 55, 119, 33);
+		editPanel.add(btnRemoveSymptom);
+		
+		JButton btnDelIll = new JButton("Delete Illness");
+		btnDelIll.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//delete the selected illness
+				Object rmIll = illList.getSelectedValue();
+			}
+		});
+		btnDelIll.setBounds(170, 55, 99, 33);
+		editPanel.add(btnDelIll);
+		
+		JLabel lblIllnesses = new JLabel("Illnesses");
+		lblIllnesses.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblIllnesses.setBounds(10, 25, 46, 14);
+		editPanel.add(lblIllnesses);
+		
+		JList sympList = new JList();
+		sympList.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		sympList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		sympList.setBounds(170, 160, 150, 181);
+		editPanel.add(sympList);
+		
+		btnRemoveSymptom.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//remove the selected symptom from the selected illness
+				Object rmSymp = sympList.getSelectedValue();
+			}
+		});
+		
+		JLabel lblSymptoms_1 = new JLabel("Symptoms");
+		lblSymptoms_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblSymptoms_1.setBounds(170, 140, 67, 14);
+		editPanel.add(lblSymptoms_1);
 		searchPanel = new JPanel();
-		patientsPanel = new JPanel();
-		profilePanel = new JPanel();
+		//patientsPanel = new JPanel();
 		tabbedPane.addTab("Search", null, searchPanel, null);
-		tabbedPane.addTab("Profile", null, profilePanel, null);
-		profilePanel.setLayout(null);
-		
-		JLabel lblName_1 = new JLabel("Name: ");
-		lblName_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblName_1.setBounds(10, 11, 300, 28);
-		profilePanel.add(lblName_1);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 73, 150, 270);
-		profilePanel.add(scrollPane);
-		
-		JTextArea textArea = new JTextArea();
-		textArea.setLineWrap(true);
-		scrollPane.setViewportView(textArea);
-		textArea.setWrapStyleWord(true);
 		
 		
 		searchPanel.setBounds(405, 25, 354, 317);
@@ -124,12 +168,12 @@ public class MedGui {
 		searchPanel.add(searchLbl);
 		
 		searchField = new JTextField();
-		searchField.setBounds(110, 54, 228, 33);
+		searchField.setBounds(110, 54, 270, 33);
 		searchPanel.add(searchField);
 		searchField.setColumns(10);
 		
 		JScrollPane resultScrlPn = new JScrollPane();
-		resultScrlPn.setBounds(110, 98, 231, 196);
+		resultScrlPn.setBounds(110, 98, 270, 196);
 		searchPanel.add(resultScrlPn);
 		
 		JTextArea txtrILikeTo = new JTextArea();
@@ -144,12 +188,12 @@ public class MedGui {
 		
 		JButton btnSearch = new JButton("Search");
 		btnSearch.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		btnSearch.setBounds(129, 307, 72, 33);
+		btnSearch.setBounds(129, 307, 88, 33);
 		searchPanel.add(btnSearch);
 		
 		JButton btnClear = new JButton("Clear");
 		btnClear.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		btnClear.setBounds(224, 307, 72, 33);
+		btnClear.setBounds(258, 307, 88, 33);
 		searchPanel.add(btnClear);
 		String[] searchOptions = {"Symptom", "Illness"};
 		JComboBox searchOptBox = new JComboBox(searchOptions);
@@ -158,49 +202,35 @@ public class MedGui {
 		searchOptBox.setBounds(110, 12, 127, 26);
 		searchPanel.add(searchOptBox);
 		
+		btnSearch.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//determine if searching for symptom or illness
+				Object option = searchOptBox.getSelectedItem();
+				if(option.equals("Symptom"))
+				{
+					
+				}
+				if(option.equals("Illness"))
+				{
+					
+				}
+				//search for it
+				String searchFor = searchField.getText();
+			}
+		});
+		
 		JLabel lblSearchOptions = new JLabel("Search for");
 		lblSearchOptions.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		lblSearchOptions.setBounds(20, 11, 80, 28);
 		searchPanel.add(lblSearchOptions);
 		recordPanel.setLayout(null);
 		
-		String[] patients = getPatients();
-		patientsPanel.setLayout(null);
-		JList patientList = new JList(patients);
-		patientList.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		patientList.setBounds(10, 11, 130, 330);
-		patientList.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-		patientList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		patientsPanel.add(patientList);
-		
-		JScrollPane scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(150, 183, 223, 160);
-		patientsPanel.add(scrollPane_1);
-		
-		JTextArea textArea_1 = new JTextArea();
-		textArea_1.setFont(new Font("Monospaced", Font.PLAIN, 13));
-		textArea_1.setWrapStyleWord(true);
-		textArea_1.setLineWrap(true);
-		scrollPane_1.setViewportView(textArea_1);
-		
-		JLabel lblNewLabel = new JLabel("Profile");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNewLabel.setBounds(150, 155, 55, 20);
-		patientsPanel.add(lblNewLabel);
-		
-		JButton btnAddSymptom = new JButton("Add Symptom");
-		btnAddSymptom.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		btnAddSymptom.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnAddSymptom.setBounds(150, 11, 110, 33);
-		patientsPanel.add(btnAddSymptom);
-		
-		JButton btnAddIllness = new JButton("Add Illness");
-		btnAddIllness.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		btnAddIllness.setBounds(150, 55, 110, 33);
-		patientsPanel.add(btnAddIllness);
+//		patientsPanel.setLayout(null);
+//		JList patientList = new JList();
+//		patientList.setBounds(10, 11, 130, 330);
+//		patientList.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+//		patientList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+//		patientsPanel.add(patientList);
 		
 		JLabel lblName = new JLabel("Name:");
 		lblName.setBounds(45, 13, 47, 20);
@@ -225,23 +255,23 @@ public class MedGui {
 					nameField.setText("");
 			}
 		});
-		clrBtn.setBounds(224, 261, 72, 33);
+		clrBtn.setBounds(258, 308, 88, 33);
 		recordPanel.add(clrBtn);
 		
 		JButton btnSave = new JButton("Save");
 		btnSave.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		btnSave.setBounds(129, 261, 72, 33);
+		btnSave.setBounds(129, 307, 88, 33);
 		btnSave.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				String symps = sympTxtArea.getText();
-				
+				String name = nameField.getText();
 				//pass the symptoms and name to the save method to be added to the database
 			}
 		});
 		recordPanel.add(btnSave);
 		
 		JScrollPane sympScrlPn = new JScrollPane();
-		sympScrlPn.setBounds(110, 52, 231, 196);
+		sympScrlPn.setBounds(110, 52, 270, 245);
 		recordPanel.add(sympScrlPn);
 		
 		sympScrlPn.setViewportView(sympTxtArea);
@@ -252,11 +282,5 @@ public class MedGui {
 
 	public JTabbedPane getTabbedPane() {
 		return tabbedPane;
-	}
-	
-	public String[] getPatients()
-	{
-		String[] p = {"Vincent, Silas", "Mersino, Chad", "Hughes, Marcus"};
-		return p;
 	}
 }
