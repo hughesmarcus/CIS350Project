21c90efdb0c88f26b00a8d9c42d3143425ea3f68
vincent-sj <vincent-sj@users.noreply.github.com>
2015-02-15 09:52:37 -0500

    Update LoginDialog.java

--- LoginDialog.java
+++ LoginDialog.java
@@ -1,22 +1,16 @@
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
-
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
-
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
-
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
-
-import org.eclipse.wb.swing.DBAccess;
-
 import java.awt.Window.Type;
 
 public class LoginDialog extends JDialog {
