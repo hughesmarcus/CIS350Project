f8ba227fac2d802929cfa54f5b24e13f88fefb0c
hughesmarcus <marcushughes80@gmail.com>
2015-04-08 13:43:23 -0400

    Update Doctor.java

--- Doctor.java
+++ Doctor.java
@@ -1,79 +1,148 @@
 import java.util.ArrayList;
+/**
+ * 
+ */
 public class Doctor {
+	/**
+	 * 
+	 */
 	private int dID;
+	/**
+	 * 
+	 */
 	private String fName;
+	/**
+	 * 
+	 */
 	private String lName;
-	private ArrayList<Patient> Patients;
+	
+	/**
+	 * 
+	 */
 	private String specialization;
 	
-	
-
-	public Doctor(int id, String first, String last)
+	/**
+	 * 
+	 */
+	private ArrayList<Patient> Patients;
+public Doctor(int id, String first, String last)
 	{
 		dID = id;
 		fName = first;
 		lName = last;
 	}
-	
-	public Doctor(int id, String first, String last, String special)
-	{
+
+	/**
+	 * 
+	 * @param id
+	 * @param special 
+	 * @param first
+	 * @param last
+	 */
+	public Doctor(final int id, final String special, final String first, final String last) {
 		dID = id;
 		fName = first;
 		lName = last;
-		specialization = special;
+		dSpecial = special;
+
 	}
 	
-	public String getSpecialization() {
-		return specialization;
+	/**
+	 * 
+	 * @return dSpecial
+	 */
+	public final String getSpecialization(){
+		return dSpecial;
 	}
-
-	public void setSpecialization(String specialization) {
+	
+	/**
+	 * 
+	 * @param special
+	 */
+	public final void setSpecialization(final String specialization){
 		this.specialization = specialization;
+		
 	}
-	
-	public int getdID() {
+
+	/**
+	 * 
+	 * @return dID
+	 */
+	public final int getdID() {
 		return dID;
 	}
-	
-	public void setdID(int dID) {
+
+	/**
+	 * 
+	 * @param dID
+	 */
+	public final void setdID(final int dID) {
 		this.dID = dID;
 	}
-	public void setdfName(String fName) {
+
+	/**
+	 * 
+	 * @param fName
+	 */
+	public void setdfName(final String fName) {
 		this.fName = fName;
 	}
-	public void setdlName(String lName) {
+
+	/**
+	 * @param lName
+	 * 
+	 */
+	public final void setdlName(final String lName) {
 		this.lName = lName;
 	}
-	
-	public String getdfName() {
+
+	/**
+	 * 
+	 * @return fName
+	 */
+	public final String getdfName() {
 		return fName;
 	}
-	public String getdlName() {
+
+	/**
+	 * 
+	 * @return lName
+	 */
+	public final String getdlName() {
 		return lName;
 	}
-	
-	public ArrayList<Patient> getPatients() {
+
+	/**
+	 * 
+	 * @return Patients
+	 */
+	public final ArrayList<Patient> getPatients() {
 		return Patients;
 	}
-	
+	/**
+	*
+	**/
 	public void setPatients(ArrayList<Patient> p)
 	{
 		Patients = p;
 	}
-	
-	public boolean addPatients(Patient pat){
+
+	/**
+	 * 
+	 * @return
+	 */
+	public final boolean addPatients(final Patient pat) {
+
 		boolean has = false;
-		for(int i = 0; i < Patients.size(); i++)
-		{
-			if(Patients.get(i) == pat)
-			{
+		for (int i = 0; i < Patients.size(); i++) {
+			if (Patients.get(i) == pat) {
 				has = true;
 			}
 		}
-		if(!has)
-		{
+		if (!has) {
 			Patients.add(pat);
 		}
 		return has;
+
 	}
 }
