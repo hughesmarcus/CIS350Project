fb27515505c9e386a7ece60bdd544d99879613c1
hughesmarcus <marcushughes80@gmail.com>
2015-04-16 17:03:20 -0400

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,74 +1,91 @@
-
 /**
- * @author Chad Mersino
- * CIS 350 Admin.java class for project
+ * @author Chad Mersino CIS 350 Admin.java class for project.
  *
  */
 
 public class Admin {
 	/**
-	 * 
+	 * Admin Id.
 	 */
 	private int aID;
 	/**
-	 * 
+	 * Admin FIrst name.
 	 */
 	private String fName;
 	/**
-	 * 
+	 * admin last name.
 	 */
 	private String lName;
-	
-	
+
 	/**
+	 * Admin constructor.
 	 * 
+	 * @param id
+	 *            the id of the admin who runs the database
+	 * @param first
+	 *            the first name of the admin
+	 * @param last
+	 *            the last name of the admin
 	 */
-	public Admin(final int id, String first, String last){
+	public Admin(final int id, final String first, final String last) {
 		aID = id;
 		fName = first;
 		lName = last;
-		
+
 	}
+
 	/**
-	 *@return
+	 * return admin id.
+	 * @return aID
 	 */
-	int getaID() {
-		
+	final int getaID() {
+
 		return aID;
 	}
+
 	/**
-	 *@param
+	 * set the Admin ID. 
+	 * @param aID
+	 *            the id of the admin who runs the database.
 	 */
-	public final void setaID(final int aID){
-		
+	public final void setaID(final int aID) {
+
 		this.aID = aID;
 	}
+
 	/**
-	 * 
+	 * return first name.
+	 * @return fName
 	 */
-	public final String getfName(){
-		
+	public final String getfName() {
+
 		return fName;
 	}
+
 	/**
-	 * 
+	 * set first name.
+	 * @param fName first name
 	 */
-	public void setafName(final String fName){
-		
+	public final void setafName(final String fName) {
+
 		this.fName = fName;
 	}
+
 	/**
-	 * 
+	 * return last name.
+	 * @return lName
 	 */
-	public final String getlName(){
-		
+	public final String getlName() {
+
 		return lName;
 	}
+
 	/**
-	 * 
+	 * set last name.
+	 * @param lName last name of admin.
 	 */
-	public void setalName(final String lName){
-		
+	public final void setalName(final String lName) {
+
 		this.lName = lName;
 	}
 }
